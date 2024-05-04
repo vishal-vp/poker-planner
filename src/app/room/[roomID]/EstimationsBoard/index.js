21f7d54135d86ac26d353e92094b90575f3c4abb
Vishal V Poojary <vishalvp0942@gmail.com>
2024-05-04 15:04:14 +0530

    Style rooms page

--- src/app/room/[roomID]/EstimationsBoard/index.js
+++ src/app/room/[roomID]/EstimationsBoard/index.js
@@ -1,13 +1,30 @@
+import { List, Table, Typography } from "antd";
+import styles from "./index.module.css";
+import Item from "antd/es/list/Item";
+
+const { Text } = Typography;
+
 export default function EstimationsBoard({ roomData }) {
-  return Object.keys(roomData?.users).map((userID) => {
-    const userEstimate = roomData?.estimates?.[userID] || "Not estimated yet";
-    return (
-      <div key={userID}>
-        <span>
-          {roomData?.users?.[userID]?.username}:{" "}
-          {roomData?.isVisible ? userEstimate : "X"}
-        </span>
-      </div>
-    );
-  });
+  const columns = [
+    {
+      title: "User",
+      render: (userID) => roomData?.users?.[userID]?.username,
+    },
+    {
+      title: "Estimate",
+      render: (userID) => {
+        const userEstimate =
+          roomData?.estimates?.[userID] || "Not estimated yet";
+        return roomData?.isVisible ? userEstimate : "X";
+      },
+    },
+  ];
+  return (
+    <Table
+      pagination={false}
+      dataSource={Object.keys(roomData?.users)}
+      columns={columns}
+      className={styles.estimationsBoard}
+    />
+  );
 }
--- src/app/room/[roomID]/page.js
+++ src/app/room/[roomID]/page.js
@@ -12,6 +12,7 @@ import { getUserInfo } from "@/utils";
 import { Button } from "antd";
 import { useEffect, useState } from "react";
 import EstimationsBoard from "./EstimationsBoard";
+import styles from "./page.module.css";
 
 export default function Room({ params }) {
   const [roomData, setRoomData] = useState({});
@@ -33,33 +34,37 @@ export default function Room({ params }) {
   }, [roomID, userInfo]);
 
   return (
-    <>
-      <Button
-        onClick={() => {
-          setEstimateVisibility(roomID, false);
-          clearEstimates(roomID);
-        }}
-        disabled={!isOwner}
-        danger
-      >
-        Delete Estimates
-      </Button>
-      <Button
-        onClick={() => setEstimateVisibility(roomID, !isVisible)}
-        disabled={!isOwner}
-      >{`${isVisible ? "Hide" : "Show"} Estimates`}</Button>
-      {ALLOWED_STORY_POINTS?.map((storyPoint) => {
-        return (
-          <Button
-            type={currentEstimate === storyPoint ? "primary" : "default"}
-            key={storyPoint}
-            onClick={() => addEstimate(roomID, userID, storyPoint)}
-          >
-            {storyPoint}
-          </Button>
-        );
-      })}
+    <div className={styles.room}>
+      <div className={styles.roomControls}>
+        <Button
+          onClick={() => {
+            setEstimateVisibility(roomID, false);
+            clearEstimates(roomID);
+          }}
+          disabled={!isOwner}
+          danger
+        >
+          Delete Estimates
+        </Button>
+        <Button
+          onClick={() => setEstimateVisibility(roomID, !isVisible)}
+          disabled={!isOwner}
+        >{`${isVisible ? "Hide" : "Show"} Estimates`}</Button>
+      </div>
+      <div className={styles.allowedStoryPointsContainer}>
+        {ALLOWED_STORY_POINTS?.map((storyPoint) => {
+          return (
+            <Button
+              type={currentEstimate === storyPoint ? "primary" : "default"}
+              key={storyPoint}
+              onClick={() => addEstimate(roomID, userID, storyPoint)}
+            >
+              {storyPoint}
+            </Button>
+          );
+        })}
+      </div>
       {roomData?.users && <EstimationsBoard roomData={roomData} />}
-    </>
+    </div>
   );
 }
