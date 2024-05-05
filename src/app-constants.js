5670d955b3d00483b871f2b848da80bc937fcba5
Vishal V Poojary <vishalvp0942@gmail.com>
2024-05-05 12:38:40 +0530

    NextJS sucks

--- src/app-constants.js
+++
@@ -1,8 +0,0 @@
-export const USER_INFO_LOCALSTORAGE_KEY = "poker-planner-user-info";
-
-export const PATHS = {
-  HOME: "/",
-  ROOM: "/room",
-};
-
-export const ALLOWED_STORY_POINTS = ["?", "1", "2", "3", "5", "8", "13", "21"];
